9833206a127417fd30d01822484424e24541f85a
Dmitriy Malyutin <37380865+Mitra92@users.noreply.github.com>
2022-10-11 16:56:36 +0300

    Update insert_rate.sql

--- sql/insert_rate.sql
+++ sql/insert_rate.sql
@@ -1,7 +1,6 @@
 INSERT INTO
-    rub_to_usd
-VALUES ('{{ params.base_currency }}', '{{ params.currency }}', {{ ti.xcom_pull(task_ids="get_rate") }}, '{{ execution_date.strftime("%Y-%m-%d") }}')
+    exchange_rates
+VALUES ('{{ params.base_currency }}', '{{ params.currency }}', {{ ti.xcom_pull(task_ids=params.get_rate_task_id) }}, '{{ execution_date.strftime("%Y-%m-%d") }}')
 ON CONFLICT (base, currency, date) DO
     UPDATE
         SET rate = excluded.rate;
-
